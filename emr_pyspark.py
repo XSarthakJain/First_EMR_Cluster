a34e80cf2872e14e79826b851a479325189b1ecc
unknown <sarthakjain31797@gmail.com>
2023-08-19 17:51:58 +0530

    feat: more than 10 quamtity records are considerable

--- emr_pyspark.py
+++ emr_pyspark.py
@@ -3,5 +3,7 @@ spark = SparkSession.builder.getOrCreate()
 input_file = 's3://myemrbucket13/inputfolder/product_data.csv'
 output_file = 's3://myemrbucket13/outputfolder/'
 df = spark.read.format('csv').option('header','true').option('inferSchema','true').option('path',input_file).load()
-df.write.format('json').option('path',output_file).option('header','true').mode('overwrite').save()
+df1 = df.select(df['*']).filter(df['quantity'] > 10)
+df1.write.format('json').option('path',output_file).option('header','true').mode('overwrite').save()
 spark.stop()
+
